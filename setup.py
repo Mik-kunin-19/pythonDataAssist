6cfd7d18087cd204049d50e593ffb2e5b25aac29
Mik-kunin-19 <122996070+Mik-kunin-19@users.noreply.github.com>
2025-07-11 19:38:37 +0200

    -c setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='pythonDataAssist',
+  version='0.1.0',
+  packages=find_packages(),
+  description='Utility and Supplementary functions for scientific workflows and markdown reports',
+  author='Mikhail Kunin',
+  license='BSD-3-Clause',
+  url='https://github.com/YOUR_USERNAME/metabolomics-utils',
+)
