8ea2b738c0a1675b30cbe241225c7166d3d02711
Mik-kunin-19 <122996070+Mik-kunin-19@users.noreply.github.com>
2025-07-11 15:27:26 +0200

    -u 1 __init__ updated adress

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,4 +5,4 @@ Utility functions for data integration and attribute inspection.
 
 # Optional: Import key functions for easier access
 from .smart_transpose import IntegrateAndPrepareDf
-from .attribute_tree import attribute_tree
+from .attr_tree import attribute_tree
